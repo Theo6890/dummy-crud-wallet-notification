b05fe58b5f134bbe90aeea966a4bde083d4a51cd
Théo RICHARD <mail@rcode.tech>
2022-07-08 18:50:40 +0100

    feat: include ShowNotifications into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from "./components/TaskManager/TaskManager";
+import ShowNotifications from "./components/Notifications/ShowNotifications";
 
 function App() {
 
   return (
     <div className="app">
+      <ShowNotifications />
       <Home />
     </div>
   );
